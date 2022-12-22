51e02f325ed45b59d21c9c807f0d09d34abf9416
lukaswoellhaf <lukas.woellaf@googlemail.com>
2022-12-22 11:45:22 +0100

    My first commit

---
+++ lib/global/styles.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+const categoryText = TextStyle(
+  color: secondaryColor,
+  fontWeight: FontWeight.bold,
+  fontSize: 18.0,
+);
+
+const titleText = TextStyle(
+  color: textColorStandard,
+  fontWeight: FontWeight.bold,
+  fontSize: 18.0,
+);
+
+const textStandard = TextStyle(
+  color: textColorStandard,
+  fontSize: 16.0,
+);
+
+const textStandardInverted = TextStyle(
+  color: textColorStandardInverted,
+  fontSize: 16.0,
+);
+
+const textErrorStandard = TextStyle(
+  color: errorColor,
+  fontSize: 13.0,
+);
+
+const textButtonText = TextStyle(
+    color: secondaryColor,
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    decoration: TextDecoration.underline
+);
+
+const textSoftSmall = TextStyle(
+    color: unselectedColor,
+    fontSize: 12
+);
+
+// Colors
+const primaryColor = Color(0xFF212121);
+const secondaryColor = Color(0xFFFE88A2);
+const backgroundColor = Color(0xFF212121);
+const cardColor = Color(0xFF393939);
+const buttonColorStandard = Color(0xFFFE88A2);
+const textColorStandard = Colors.white;
+const textColorStandardInverted = Colors.black;
+const unselectedColor= Color(0xB3FFFFFF);
+const disabledColor= Color(0x80FFFFFF);
+const hintColor= Color(0x80FFFFFF);
+const errorColor = Color(0xFFF44336);
+const successColor = Color(0xFF4CAF50);
+const warningColor = Color(0xFFFF9800);
+const infoColor = Color(0xFF2196F3);
+const dividerColor = Color(0x80000000);
---
+++ lib/widget/button/custom_card_button.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+import '../../global/styles.dart';
+
+class CustomCardButton extends StatefulWidget {
+  const CustomCardButton({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _CustomCardButtonState();
+}
+
+class _CustomCardButtonState extends State<CustomCardButton> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Card(
+// clipBehavior is necessary because, without it, the InkWell's animation
+// will extend beyond the rounded edges of the [Card] (see https://github.com/flutter/flutter/issues/109776)
+// This comes with a small performance cost, and you should not set [clipBehavior]
+// unless you need it.
+        clipBehavior: Clip.hardEdge,
+        child: InkWell(
+          splashColor: buttonColorStandard,
+          onTap: () {
+            debugPrint('Card tapped.');
+          },
+          child: SizedBox(
+            width: 125,
+            height: 125,
+            child: Container(
+              decoration: BoxDecoration(
+                  border: Border.all(color: secondaryColor, width: 5.0, style: BorderStyle.solid),
+                  image: const DecorationImage(
+                      opacity: 0.7,
+                      image: AssetImage("assets/netflix-icon.png"),
+                      fit: BoxFit.cover)),
+              child: const Center(
+                child: Text('NETFLIX',
+                    textAlign: TextAlign.center, style: textStandard),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// Container(
+// width: 150,
+// height: 150,
+// decoration: const BoxDecoration(
+// image: DecorationImage(
+// image: AssetImage("assets/netflix-icon.png"),
+// fit: BoxFit.cover)),
+
+// GestureDetector(
+// child: Card(
+// elevation: 4.0,
+// // clipBehavior is necessary because, without it, the InkWell's animation
+// // will extend beyond the rounded edges of the [Card] (see https://github.com/flutter/flutter/issues/109776)
+// // This comes with a small performance cost, and you should not set [clipBehavior]
+// // unless you need it.
+// clipBehavior: Clip.hardEdge,
+// child: InkWell(
+// splashColor: buttonColorStandard,
+// onTap: () {
+// debugPrint('Card tapped.');
+// },
+// child: const Image(
+// image: AssetImage('assets/netflix-icon.png'),
+// fit: BoxFit.cover,
+// width: 125,
+// height: 125,
+// opacity: AlwaysStoppedAnimation(0.4))),
+// ),
+// );
+
+// GestureDetector(
+// child: Card(
+// // clipBehavior is necessary because, without it, the InkWell's animation
+// // will extend beyond the rounded edges of the [Card] (see https://github.com/flutter/flutter/issues/109776)
+// // This comes with a small performance cost, and you should not set [clipBehavior]
+// // unless you need it.
+// clipBehavior: Clip.hardEdge,
+// child: InkWell(
+// splashColor: buttonColorStandard,
+// onTap: () {
+// debugPrint('Card tapped.');
+// },
+// child: const SizedBox(
+// width: 125,
+// height: 125,
+// child: Center(
+// child: Text(
+// 'A card that can be tapped',
+// textAlign: TextAlign.center,
+// style: textStandardInverted
+// ),
+// ),
+// ),
+// ),
+// ),
+// );
+
+// Container(
+// width: 150,
+// height: 150,
+// decoration: const BoxDecoration(
+// image: DecorationImage(
+// image: AssetImage("assets/netflix-icon.png"),
+// fit: BoxFit.cover)),
