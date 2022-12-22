b064d9747ccd287d76c0335bff8926c7a4b1e84a
lukaswoellhaf <lukas.woellhaf@googlemail.com>
2022-12-22 17:28:55 +0100

    Finished custom_card_button.dart

--- lib/widget/button/custom_card_button.dart
+++ lib/widget/button/custom_card_button.dart
@@ -5,6 +5,10 @@ import '../../global/styles.dart';
 class CustomCardButton extends StatefulWidget {
   const CustomCardButton({super.key});
 
+/*  final String cardText;
+  final String cardImagePath;*/
+
+
   @override
   State<StatefulWidget> createState() => _CustomCardButtonState();
 }
@@ -14,10 +18,10 @@ class _CustomCardButtonState extends State<CustomCardButton> {
   Widget build(BuildContext context) {
     return GestureDetector(
       child: Card(
-// clipBehavior is necessary because, without it, the InkWell's animation
-// will extend beyond the rounded edges of the [Card] (see https://github.com/flutter/flutter/issues/109776)
-// This comes with a small performance cost, and you should not set [clipBehavior]
-// unless you need it.
+        // clipBehavior is necessary because, without it, the InkWell's animation
+        // will extend beyond the rounded edges of the [Card] (see https://github.com/flutter/flutter/issues/109776)
+        // This comes with a small performance cost, and you should not set [clipBehavior]
+        // unless you need it.
         clipBehavior: Clip.hardEdge,
         child: InkWell(
           splashColor: buttonColorStandard,
@@ -25,14 +29,18 @@ class _CustomCardButtonState extends State<CustomCardButton> {
             debugPrint('Card tapped.');
           },
           child: SizedBox(
-            width: 125,
-            height: 125,
+            width: 115,
+            height: 115,
             child: Container(
               decoration: BoxDecoration(
-                  border: Border.all(color: secondaryColor, width: 5.0, style: BorderStyle.solid),
+                color: secondaryColor.withAlpha(50),
+                  border: Border.all(
+                      color: secondaryColor,
+                      width: 5.0,
+                      style: BorderStyle.solid),
                   image: const DecorationImage(
-                      opacity: 0.7,
-                      image: AssetImage("assets/netflix-icon.png"),
+                      opacity: 0.8,
+                      image: AssetImage("assets/adventures.png"),
                       fit: BoxFit.cover)),
               child: const Center(
                 child: Text('NETFLIX',
